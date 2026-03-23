7c8abdcca015165ff86747ffc01c81b82c798b77
M Razif Rizqullah <razifrizqullah@gmail.com>
2026-03-23 12:05:20 +0700

    Hide play/terminal buttons in Completed column and restrict edit button to Todo only

--- src/lib/components/TaskCard.svelte
+++ src/lib/components/TaskCard.svelte
@@ -102,7 +102,7 @@
       <button class="btn-icon play" title="Move to In Progress" disabled={isMoving} on:click={handleMoveToInProgress}>
         <svg width="12" height="12" viewBox="0 0 12 12" fill="currentColor"><path d="M2 1.5l9 4.5-9 4.5V1.5z"/></svg>
       </button>
-    {:else}
+    {:else if task.column !== "Completed"}
       <button class="btn-icon play" class:active={isRunning} title={isRunning ? "Stop" : "Run"}
         disabled={isPlaying || isResetting} on:click={handlePlay}>
         {#if isRunning}
@@ -119,12 +119,14 @@
       </button>
     {/if}
     <div class="actions">
-      <button class="btn-icon" title="Edit" on:click={() => (showEditModal = true)}>
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
-          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-        </svg>
-      </button>
+      {#if task.column === "Todo"}
+        <button class="btn-icon" title="Edit" on:click={() => (showEditModal = true)}>
+          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+          </svg>
+        </button>
+      {/if}
       <button class="btn-icon delete" title="Delete" disabled={isDeleting}
         on:click={() => (showConfirmDelete = true)}>
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
